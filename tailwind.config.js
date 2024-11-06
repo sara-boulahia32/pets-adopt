05379bf3a5c303a6e1cd04813cbb96ebdb041467
sara boulahia <saraboulahia32@gmail.com>
2024-11-06 14:12:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#34353A',
+        customLightWight: '#3D3E42',
+       
+      },
+    },
+  },
+  plugins: [],
+}
+
